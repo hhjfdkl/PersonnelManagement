9ee70231d8f2ebf5b63a118ec8297ae0cac0115e
hhjfdkl <nate.koroman@gmail.com>
2024-07-05 07:24:52 -0400

    Completed core functionality. Just finishing up on CSS

--- personnel-management-frontend/src/app/api-components/employee/employee-detail/employee-detail.component.ts
+++ personnel-management-frontend/src/app/api-components/employee/employee-detail/employee-detail.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HttpService } from '../../../services/http.service';
+import { Department } from '../../../models/department';
+import { Office } from '../../../models/office';
 
 @Component({
   selector: 'app-employee-detail',
@@ -21,6 +23,12 @@ export class EmployeeDetailComponent {
   //for holding and modeling employee updates
   updatedEmployee: Employee = new Employee(0,'','',null,null);
 
+  //for holding existing departments to add
+  updatedDepartment: Department = new Department(0,'',[]);
+
+  //for holding existing offices to add
+  updatedOffice: Office = new Office(0, '','','','','',0,0,[]);
+
   //Activated Route and Http Service so we can get access to values of the route and the url
   constructor(private route: ActivatedRoute, private httpService: HttpService) 
   {
@@ -37,8 +45,9 @@ export class EmployeeDetailComponent {
       this.updatedEmployee.employeeId = data.body.employeeId;
       this.updatedEmployee.firstName = data.body.firstName;
       this.updatedEmployee.lastName = data.body.lastName;
-      this.updatedEmployee.department = data.body.department;
-      this.updatedEmployee.office = data.body.office;
+      this.updatedEmployee.department = this.updatedDepartment;
+      this.updatedEmployee.office = this.updatedOffice;
+      
     });
   }
 
@@ -52,5 +61,116 @@ export class EmployeeDetailComponent {
     })
   }
 
+  //getters for office and dept
+  // getOfficeById()
+  // {
+  //   this.httpService
+  //   .getOfficeById(this.route.snapshot.params['id'])
+  //   .subscribe(data => {
+  //     this.updatedOffice.officeId = data.body.officeId;
+  //     this.updatedOffice.officeName = data.body.officeName;
+  //     this.updatedOffice.address = data.body.address;
+  //     this.updatedOffice.suite = data.body.suite;
+  //     this.updatedOffice.city = data.body.city;
+  //     this.updatedOffice.state = data.body.state;
+  //     this.updatedOffice.zip = data.body.zip;
+  //     this.updatedOffice.maxCapacity = data.body.maxCapacity;
+  //     // this.updatedOffice.employees = data.body.employees;
+  //   })
+  // }
+    //dept getter so we can grab department and set to employee
+
+    formDepartment: Department = new Department(0,'',[]); //for our add department method
+    formDepartmentId: number = 0; //base value for our get dept by id form
+    departmentNotThere: boolean = false; //bool to change to true if we don't find anything from our db
+    departmentSelected: boolean = false; //bool to change when dept is selected
+
+    getDepartmentById() 
+    {
+      this.httpService.getDepartmentById(this.formDepartmentId).subscribe(
+        { //using next / error in case we don't get anything back
+          next: data => {
+            // this.departments = [];
+            // this.departments.push(data.body);
+            this.departmentSelected = true;
+            this.formDepartment = data.body;
+            this.departmentNotThere = false;
+            console.log('successfully pulled department by Id');
+          },
+          error: error => {
+            console.log(error);
+            this.departmentSelected = false;
+            this.departmentNotThere = true;
+          },
+          complete: () => console.log('Completed') //puts message to console when we successfully get a response
+        }
+      );
+    }
+
+      //to set data acquired from getter method for dept to our employee
+    setFormEmployeeDepartment()
+    {
+      
+      
+        this.updatedEmployee.department.departmentId = this.formDepartment.departmentId;
+        this.updatedEmployee.department.departmentName = this.formDepartment.departmentName;
+        this.updatedEmployee.department.employees = this.formDepartment.employees;
+        console.log(this.formDepartment.departmentName);
+      
+      
+    }
+
+    formOffice: Office = new Office(0,'','','','','',0,0,[]); //for our add office method
+    formOfficeId: number = 0; //base value for our get office by id form
+    officeNotThere: boolean = false; //bool to change to true if we don't find anything from our db
+    officeSelected: boolean = false; //bool to change when office is selected
+
+    getOfficeById() 
+    {
+      this.httpService.getOfficeById(this.formOfficeId).subscribe(
+        { //using next / error in case we don't get anything back
+          next: data => {
+            // this.offices = [];
+            // this.offices.push(data.body);
+            this.officeSelected = true;
+            this.formOffice = data.body;
+            this.officeNotThere = false;
+            console.log('successfully pulled office by Id');
+          },
+          error: error => {
+            console.log(error);
+            this.officeSelected = false;
+            this.officeNotThere = true;
+          },
+          complete: () => console.log('Completed') //puts message to console when we successfully get a response
+        }
+      );
+    }
+
+    //to set data acquired from getter method for office to our employee
+  setFormEmployeeOffice()
+  {
+      this.updatedEmployee.office.officeId = this.formOffice.officeId;
+      this.updatedEmployee.office.officeName = this.formOffice.officeName;
+      this.updatedEmployee.office.address = this.formOffice.address;
+      this.updatedEmployee.office.suite = this.formOffice.suite;
+      this.updatedEmployee.office.city = this.formOffice.city;
+      this.updatedEmployee.office.state = this.formOffice.state;
+      this.updatedEmployee.office.zip = this.formOffice.zip;
+      this.updatedEmployee.office.maxCapacity = this.formOffice.maxCapacity;
+      this.updatedEmployee.office.employees = this.formOffice.employees;
+    
+    
+  }
+
+  // getDepartmentById()
+  // {
+  //   this.httpService
+  //   .getDepartmentById(this.route.snapshot.params['id'])
+  //   .subscribe(data => {
+  //     this.updatedDepartment.departmentId = data.body.departmentId;
+  //     this.updatedDepartment.departmentName = data.body.departmentName;
+  //   });
+  // }
 
 }
--- personnel-management-frontend/src/app/api-components/employee/employees/employees.component.ts
+++ personnel-management-frontend/src/app/api-components/employee/employees/employees.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { EmployeeCardComponent } from '../employee-card/employee-card.component';
 import { FormsModule } from '@angular/forms';
 import { HttpService } from '../../../services/http.service';
+import { Office } from '../../../models/office';
+import { Department } from '../../../models/department';
 
 @Component({
   selector: 'app-employees',
@@ -18,9 +20,29 @@ export class EmployeesComponent {
   employeeIds: number[] = []; 
   employeeNames: string[] = [];
 
+  departments: Department[] = [];
+  departmentIds: number[] = [];
+  departmentNames: string[] = [];
+
+  offices: Office[] = [];
+  officeIds: number[] = [];
+  officeNames: string[] = [];
+
   
 
-  formEmployee: Employee = new Employee(0,'','',null,null); //for our add employee method
+  formOffice: Office = new Office(0,'','','','','',0,0,[]); //for our add office method
+  formOfficeId: number = 0; //base value for our get office by id form
+  officeNotThere: boolean = false; //bool to change to true if we don't find anything from our db
+  officeSelected: boolean = false; //starts false for our display
+
+  formDepartment: Department = new Department(0,'',[]); //for our add department method
+  formDepartmentId: number = 0; //base value for our get dept by id form
+  departmentNotThere: boolean = false; //bool to change to true if we don't find anything from our db
+  departmentSelected: boolean = false;
+
+  
+
+  formEmployee: Employee = new Employee(0,'','',this.formDepartment,this.formOffice); //for our add employee method
   formEmployeeId: number = 0; //base value for our get dept by id form
   employeeNotThere: boolean = false; //bool to change to true if we don't find anything from our db
 
@@ -28,6 +50,83 @@ export class EmployeesComponent {
   {
     this.getAllEmployees();
   }
+
+  //to set data acquired from getter method for dept to our employee
+  setFormEmployeeDepartment()
+  {
+     
+    
+      this.formEmployee.department.departmentId = this.formDepartment.departmentId;
+      this.formEmployee.department.departmentName = this.formDepartment.departmentName;
+      this.formEmployee.department.employees = this.formDepartment.employees;
+      console.log(this.formDepartment.departmentName);
+    
+    
+  }
+
+//to set data acquired from getter method for office to our employee
+  setFormEmployeeOffice()
+  {
+      this.formEmployee.office.officeId = this.formOffice.officeId;
+      this.formEmployee.office.officeName = this.formOffice.officeName;
+      this.formEmployee.office.address = this.formOffice.address;
+      this.formEmployee.office.suite = this.formOffice.suite;
+      this.formEmployee.office.city = this.formOffice.city;
+      this.formEmployee.office.state = this.formOffice.state;
+      this.formEmployee.office.zip = this.formOffice.zip;
+      this.formEmployee.office.maxCapacity = this.formOffice.maxCapacity;
+      this.formEmployee.office.employees = this.formOffice.employees;
+    
+    
+  }
+
+  //dept getter so we can grab department and set to employee
+  getDepartmentById() 
+  {
+    this.httpService.getDepartmentById(this.formDepartmentId).subscribe(
+      { //using next / error in case we don't get anything back
+        next: data => {
+          // this.departments = [];
+          // this.departments.push(data.body);
+          this.departmentSelected = true;
+          this.formDepartment = data.body;
+          this.departmentNotThere = false;
+          console.log('successfully pulled department by Id');
+        },
+        error: error => {
+          this.departmentSelected = false;
+          console.log(error);
+          this.departmentNotThere = true;
+        },
+        complete: () => console.log('Completed') //puts message to console when we successfully get a response
+      }
+    );
+  }
+
+  //office getter so we can grab office and set to employee
+  getOfficeById() 
+  {
+    this.httpService.getOfficeById(this.formOfficeId).subscribe(
+      { //using next / error in case we don't get anything back
+        next: data => {
+          // this.offices = [];
+          // this.offices.push(data.body);
+          this.officeSelected = true;
+          this.formOffice = data.body;
+          this.officeNotThere = false;
+          console.log('successfully pulled office by Id');
+        },
+        error: error => {
+          this.officeSelected = false;
+          console.log(error);
+          this.officeNotThere = true;
+        },
+        complete: () => console.log('Completed') //puts message to console when we successfully get a response
+      }
+    );
+  }
+
+  
 
   //CREATE / POST
   addEmployee()
@@ -37,7 +136,11 @@ export class EmployeesComponent {
     .subscribe(data => {
       this.getAllEmployees();
     });
+    // this.setFormEmployeeDepartment();
+    // this.setFormEmployeeOffice();
   }
+
+  
 
   //READ / GET
   getAllEmployees()
